4095b4ec99b240c86d731103877b524847bf96de
Crius <Crius@Crius-PC>
2014-10-07 13:05:23 +0200

    Update1

--- Icarasweb/src/main/java/nl/rsvier/icaras/core/relatiebeheer/Werknemer.java
+++ Icarasweb/src/main/java/nl/rsvier/icaras/core/relatiebeheer/Werknemer.java
@@ -2,23 +2,24 @@ package nl.rsvier.icaras.core.relatiebeheer;
 
 import java.util.Set;
 
+import javax.persistence.ElementCollection;
 import javax.persistence.Embeddable;
 
 
 @Embeddable
 public class Werknemer implements IPersoonsRol{
 
-	
+	@ElementCollection
 	private Set <Arbeidsovereenkomst> Arbeidsovereenkomsten;
 
 	
-	
-	
+//methodes
 	
 	public Set<Arbeidsovereenkomst> getArbeidsovereenkomsten() {
 		return Arbeidsovereenkomsten;
 	}
-
+	
+	
 	public void setArbeidsovereenkomsten(
 			Set<Arbeidsovereenkomst> arbeidsovereenkomsten) {
 		Arbeidsovereenkomsten = arbeidsovereenkomsten;
